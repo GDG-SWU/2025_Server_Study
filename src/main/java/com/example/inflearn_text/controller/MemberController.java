81205bf18901bf1e4b79a04dfe59447aa1e8e18c
SungMinju <minju2928@naver.com>
2025-09-29 06:58:46 +0900

    @Component 스캔으로 자동 의존관계 설정

---
+++ src/main/java/com/example/inflearn_text/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.example.inflearn_text.controller;
+
+import com.example.inflearn_text.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+}
--- src/main/java/com/example/inflearn_text/service/MemberService.java
+++ src/main/java/com/example/inflearn_text/service/MemberService.java
@@ -3,14 +3,22 @@ package com.example.inflearn_text.service;
 import com.example.inflearn_text.domain.Member;
 import com.example.inflearn_text.repository.MemberRepository;
 import com.example.inflearn_text.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Service
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository=memberRepository;
+    }
 
     //회원가입
     public Long join(Member member){
