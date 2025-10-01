61026141ec4dc77acf40f60e0a8cfeeea8d0c37c
SungMinju <minju2928@naver.com>
2025-10-01 18:15:24 +0900

    회원웹기능-등록,조회

--- src/main/java/com/example/inflearn_text/controller/MemberController.java
+++ src/main/java/com/example/inflearn_text/controller/MemberController.java
@@ -1,8 +1,16 @@
 package com.example.inflearn_text.controller;
 
+
+import com.example.inflearn_text.domain.Member;
+import com.example.inflearn_text.domain.MemberForm;
 import com.example.inflearn_text.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MemberController {
@@ -12,6 +20,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
