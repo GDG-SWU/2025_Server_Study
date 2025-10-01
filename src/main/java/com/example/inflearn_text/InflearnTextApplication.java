417cc5cb620a4730101a130de7faa1d57448c41b
SungMinju <minju2928@naver.com>
2025-10-01 18:14:37 +0900

    test 코드 작성

--- src/main/java/com/example/inflearn_text/InflearnTextApplication.java
+++ src/main/java/com/example/inflearn_text/InflearnTextApplication.java
@@ -2,8 +2,10 @@ package com.example.inflearn_text;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InflearnTextApplication {
 
 	public static void main(String[] args) {
