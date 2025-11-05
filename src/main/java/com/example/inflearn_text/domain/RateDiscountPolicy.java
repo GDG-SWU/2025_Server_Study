bb17c3d257aeaff178d1bd1c68b90ab69282008c
SungMinju <minju2928@naver.com>
2025-11-06 04:26:16 +0900

    fix: 코드의 안정호를 위해 수정

--- src/main/java/com/example/inflearn_text/domain/RateDiscountPolicy.java
+++ src/main/java/com/example/inflearn_text/domain/RateDiscountPolicy.java
@@ -1,12 +1,12 @@
 package com.example.inflearn_text.domain;
 
 public class RateDiscountPolicy implements DiscountPolicy{
-    private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price){
         if(member.getGrade() == Grade.VIP){
-            return price*discountPercent / 100;
+            int discountPercent = 10;
+            return price* discountPercent / 100;
         }
         else {
             return 0;
