38287a3589c3ba9ea983b74c48db66c55d4e74a1
SungMinju <minju2928@naver.com>
2025-11-06 04:23:18 +0900

    fix: 새로운 할인 정책 적용으로 RateDiscountPolicy 로 연결고리 수정

--- src/main/java/com/example/inflearn_text/service/OrderServiceImpl.java
+++ src/main/java/com/example/inflearn_text/service/OrderServiceImpl.java
@@ -1,16 +1,14 @@
 package com.example.inflearn_text.service;
 
-import com.example.inflearn_text.domain.DiscountPolicy;
-import com.example.inflearn_text.domain.FixDiscountPolicy;
-import com.example.inflearn_text.domain.Member;
-import com.example.inflearn_text.domain.Order;
+import com.example.inflearn_text.domain.*;
 import com.example.inflearn_text.repository.MemberRepository;
 import com.example.inflearn_text.repository.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
